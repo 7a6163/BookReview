d419525148910fd7687967eac52e87a8719fbb07
Zac <zac_wang@tty.com.tw>
2013-02-27 17:44:36 +0800

    BookCrawler 2.0

--- BookCrawler/Form1.Designer.cs
+++ BookCrawler/Form1.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                _bookEntities.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -29,11 +30,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 109);
+            this.button1.Location = new System.Drawing.Point(95, 140);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +44,40 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(95, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- BookCrawler/Form1.cs
+++ BookCrawler/Form1.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Globalization;
-using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using HtmlAgilityPack;
 using HtmlDocument = HtmlAgilityPack.HtmlDocument;
-using Domain; 
+using Domain;
 
 namespace BookCrawler
 {
@@ -25,40 +21,87 @@ namespace BookCrawler
 
         private void button1_Click(object sender, EventArgs e)
         {
-            HtmlWeb webClient = new HtmlWeb()
+            var webClient = new HtmlWeb
                 {
                     AutoDetectEncoding = false,
                     OverrideEncoding = Encoding.GetEncoding(950)
                 };
 
-            int start = 0010569456;
-            int end = 0010569460;
+            const int start = 0010569456;
+            const int end = 0010569460;
 
-            string dateFormat = "yyyy年MM月dd日";
+            const string dateFormat = "yyyy年MM月dd日";
             for (int i = start; i < end; i++)
             {
                 string url = string.Format("http://www.books.com.tw/exep/prod/booksfile.php?item={0}",
                                            i.ToString(CultureInfo.InvariantCulture).PadLeft(10, '0'));
-                HtmlDocument doc =  webClient.Load(url);
+                HtmlDocument doc = webClient.Load(url);
 
-                var count = doc.DocumentNode.SelectNodes(@"//*[@id=""pr_data""]/ul[1]/li").Count;
+                if (doc.DocumentNode.SelectNodes(@"//*[@id=""pr_data""]/ul[1]/li") == null) break;
 
-                string dateString =
-                    doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/dfn") != null
-                        ? doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/dfn").InnerText
-                        : doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[5]/dfn").InnerText;
+                int count = doc.DocumentNode.SelectNodes(@"//*[@id=""pr_data""]/ul[1]/li").Count;
 
-                Books book = new Books();
+                var book = new Books
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/div[1]/h1/span").InnerText,
+                        Author = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[1]/a").InnerText
+                    };
+                string sUrl = doc.DocumentNode.SelectSingleNode(@"//*[@id=""main_img""]")
+                                   .GetAttributeValue("src", string.Empty);
 
-                book.Id = Guid.NewGuid();
-                book.Name = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/div[1]/h1/span").InnerText;
-                book.Author = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[1]/a").InnerText;
-                book.Publisher = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[2]/a") != null
-                                     ? doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[2]/a")
-                                          .InnerText
-                                     : doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/a")
-                                          .InnerText;
+                var regex = new Regex("image=(?<url>.+jpg)", RegexOptions.None);
+                var matches = regex.Match(sUrl);
+                string imgUrl =  matches.Groups["url"].Value;
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(imgUrl, string.Format("{0}.jpg", book.Id));
+                }
 
+                
+                string price =
+                    doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/div[2]/div/ul/li[1]/dfn[1]/u").InnerText;
+                book.Price = int.Parse(price);
+
+                string dateString = string.Empty;
+                string language = string.Empty;
+                switch (count)
+                {
+                    case 7:
+                        dateString = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[5]/dfn").InnerText;
+                        book.Publisher = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/a").InnerText;
+                        book.ISBN =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[6]/dfn[2]").InnerText;
+                        language =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[6]/dfn[1]").InnerText;
+                        book.Language = Equals(language, "繁體中文") ? "0" : "1";
+                        book.Binding =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[7]/dfn").InnerText;
+
+                        break;
+                    case 6:
+                        dateString = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/dfn").InnerText;
+                        book.Publisher = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[3]/a").InnerText;
+                        book.ISBN =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[5]/dfn[2]").InnerText;
+                        language =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[5]/dfn[1]").InnerText;
+                        book.Binding =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[6]/dfn").InnerText;
+                        break;
+                    case 5:
+                        dateString = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[3]/dfn").InnerText;
+                        book.Publisher = doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[2]/a").InnerText;
+                        book.ISBN =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/dfn[2]").InnerText;
+                        language =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[4]/dfn[1]").InnerText;
+                        book.Binding =
+                            doc.DocumentNode.SelectSingleNode(@"//*[@id=""pr_data""]/ul[1]/li[5]/dfn").InnerText;
+                        break;
+                }
+
+                book.Language = Equals(language, "繁體中文") ? "0" : "1";
                 book.PublishDate = DateTime.ParseExact(dateString, dateFormat, CultureInfo.InvariantCulture);
                 book.Description = "Nothing";
                 book.CreateDate = DateTime.Now;
